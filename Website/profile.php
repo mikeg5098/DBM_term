afa625a56e7394d25cc2776fe5960439411dcb98
atquick94 <33730115+atquick94@users.noreply.github.com>
2017-11-30 18:24:25 -0500

    Temporary Links

--- Website/profile.php
+++ Website/profile.php
@@ -8,6 +8,21 @@ include('session.php');
 <link href="style2.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<div id="header">
+        <div class="container">
+            <ul id="header-nav">
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="DataBaseTerm.php">DATABASE</a></li>
+                <li><a href="team.html">TEAM</a></li>
+                <li><a href="login_index.php">LOGIN</a></li>
+            </ul>
+            <div id="boxnav"></div>
+        
+        
+        </div>
+    
+    
+    </div>
 <div id="profile">
 <b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
 <b id="logout"><a href="logout.php">Log Out</a></b>
