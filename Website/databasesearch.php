facb39a53a4a6b7b063c43961075feb3259833e1
jsy15 <jsy15@zips.uakron.edu>
2017-12-01 14:21:56 -0500

    Fixed Search php to have faster speed
    fixed search

--- Website/databasesearch.php
+++ Website/databasesearch.php
@@ -3,7 +3,7 @@ error_reporting(E_ERROR);
 require_once("dbcontroller.php");
 $db_handle = new DBController();
 $search = $_GET["search"];
-$sql = "SELECT * FROM database_testing WHERE model LIKE '%" . $search . "%' OR make LIKE '%" . $search . "%' OR year LIKE '%" . $search . "%';";
+$sql = "SELECT database_testing.car_id, Model, Make, Year, rating_car FROM database_testing join (SELECT car_id, SUM(rating) as rating_car FROM DBM_ratings GROUP BY car_id) AS rating_table WHERE database_testing.car_id = rating_table.car_id AND model LIKE '%" . $search . "%' OR make LIKE '%" . $search . "%' OR year LIKE '%" . $search . "%' ORDER BY model ;";
 $faq = $db_handle->runQuery($sql);
 ?>
 <html>
@@ -45,25 +45,7 @@ $faq = $db_handle->runQuery($sql);
 				<td contenteditable="true" onBlur="saveToDatabase(this,'Make','<?php echo $faq[$k]["car_id"]; ?>')" onClick="showEdit(this);"><?php echo $faq[$k]["Make"]; ?></td>
 				<td contenteditable="true" onBlur="saveToDatabase(this,'Year','<?php echo $faq[$k]["car_id"]; ?>')" onClick="showEdit(this);"><?php echo $faq[$k]["Year"]; ?></td>
 				<td>
-					<?php
-						// Connect to MySQL
-						$db = mysqli_connect("db1.cs.uakron.edu:3306", "jsy15", "termProjJacob17");
-
-						// Select the database
-						$er = mysqli_select_db($db,"ISP_jsy15");
-
-						$temp = $faq[$k]["car_id"];
-						$query = "CALL getRatingForCar_id('" . $temp . "')"; 
-
-						$result = mysqli_query($db,$query);
-
-						$row = mysqli_fetch_array($result);
-
-						$values = array_values($row);
-						$value = htmlspecialchars($values[2 * 3 + 1]);
-						if($value == ""){print "0";}
-						else{print $value;}
-					?>
+					<?php echo $faq[$k]["rating_car"]; ?>
 				</td>
 				<td> <button onclick="deleteRecord(<?php echo $faq[$k]["car_id"]; ?>)">Delete</button>  <button onclick="setRatingID(<?php echo $faq[$k]["car_id"]; ?>)" data-toggle="modal" data-target="#ModalRating" >Insert Rating</button></td>
 			  </tr>
