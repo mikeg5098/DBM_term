7e4823030fd99d49f1f15bef22d720a73d313b43
atquick94 <33730115+atquick94@users.noreply.github.com>
2017-12-07 15:16:28 -0500

    Implemented search fix

--- Website/databasesearch.php
+++ Website/databasesearch.php
@@ -4,7 +4,7 @@ include('session.php');
 require_once("dbcontroller.php");
 $db_handle = new DBController();
 $search = $_GET["search"];
-$sql = "SELECT database_testing.car_id, Model, Make, Year, rating_car FROM database_testing join (SELECT car_id, SUM(rating) as rating_car FROM DBM_ratings GROUP BY car_id) AS rating_table WHERE database_testing.car_id = rating_table.car_id AND model LIKE '%" . $search . "%' OR make LIKE '%" . $search . "%' OR year LIKE '%" . $search . "%' GROUP BY database_testing.car_id ORDER BY model ;";
+$sql = "SELECT database_testing.car_id, Model, Make, Year, rating_car FROM database_testing join (SELECT car_id, SUM(rating) as rating_car FROM DBM_ratings GROUP BY car_id) AS rating_table WHERE database_testing.car_id = rating_table.car_id AND (model LIKE '%" . $search . "%' OR make LIKE '%" . $search . "%' OR year LIKE '%" . $search . "%') ORDER BY model ;";
 $faq = $db_handle->runQuery($sql);
 ?>
 <html>
@@ -83,9 +83,9 @@ $faq = $db_handle->runQuery($sql);
 		  foreach($faq as $k=>$v) {
 		  ?>
 			  <tr>
-				<td style="text-align:center;" contenteditable="true" onBlur="saveToDatabase(this,'Model','<?php echo $faq[$k]["car_id"]; ?>')" onClick="showEdit(this);"><?php echo $faq[$k]["Model"]; ?></td>
-				<td contenteditable="true" onBlur="saveToDatabase(this,'Make','<?php echo $faq[$k]["car_id"]; ?>')" onClick="showEdit(this);"><?php echo $faq[$k]["Make"]; ?></td>
-				<td contenteditable="true" onBlur="saveToDatabase(this,'Year','<?php echo $faq[$k]["car_id"]; ?>')" onClick="showEdit(this);"><?php echo $faq[$k]["Year"]; ?></td>
+				<td style="text-align:center;"><?php echo $faq[$k]["Model"]; ?></td>
+				<td><?php echo $faq[$k]["Make"]; ?></td>
+				<td><?php echo $faq[$k]["Year"]; ?></td>
 				<td>
 					<?php echo $faq[$k]["rating_car"]; ?>
 				</td>
